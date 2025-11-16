6b763b9ce34bf42128cd4ac0dcc4a65112dd2b66
unknown <patrickgsiqueira@hotmail.com>
2025-11-15 22:13:07 -0300

    mercado pago23

--- config/mercadoPago.js
+++ config/mercadoPago.js
@@ -1,7 +1,12 @@
-const mercadopago = require("mercadopago")
+// /config/mercadoPago.js
+
+const mercadopago = require("mercadopago");
+
+// --- CREDENCIAIS DE PRODUÇÃO HARDCODED ---
+const ACCESS_TOKEN_PRODUCAO = "APP_USR-7771336761487137-111518-d1feb7719fe0b484daae4f1c5f4cf8a9-2019519940";
 
 mercadopago.configure({
-  access_token: process.env.MERCADO_PAGO_TOKEN,
-})
+  access_token: ACCESS_TOKEN_PRODUCAO,
+});
 
-module.exports = mercadopago
+module.exports = mercadopago;
--- server.js
+++ server.js
@@ -137,7 +137,7 @@ async function iniciarServidor() {
     // Sincroniza os modelos com o banco
     // { alter: true } é seguro para desenvolvimento, pois tenta atualizar as tabelas sem apagar dados.
     await sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
-await sequelize.sync({ force: true }); // recria as tabelas
+await sequelize.sync({ force: false }); // recria as tabelas
 await sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
     console.log("Modelos sincronizados com o banco de dados.");
 
